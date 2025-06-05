1068934fa16248d4944c51baefe1dc7477d6cabe
HelloMatcha <ivanderricardo4@gmail.com>
2025-06-05 14:02:07 +0700

    Added Modal Image for Projects

--- scroll_and_cursor.js
+++ scroll_and_cursor.js
@@ -1,4 +1,4 @@
-// Scroll Trigger animation
+// Scroll Trigger Animation
 document.addEventListener("DOMContentLoaded", () => {
   const faders = document.querySelectorAll('.scroll-fade-up');
 
@@ -39,4 +39,31 @@ document.addEventListener('mousemove', function (e) {
   const light = document.querySelector('.flashlight');
   light.style.left = e.clientX + 'px';
   light.style.top = e.clientY + 'px';
+});
+
+
+//Modal Image
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImg");
+  const captionText = document.getElementById("caption");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".modal-trigger").forEach(img => {
+    img.addEventListener("click", () => {
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 });
